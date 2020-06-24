fa98739afab2db3424ad2877c2cc91e18ce07174
alexis33utu@gmail.com <alexis33utu@gmail.com>
2020-06-23 21:51:33 -0300

    Dividi en carpetas

---
+++ Documentacion/Sistemas Operativos/Scripts/InteraccionConCentOS/EntornoDeTrabajo/Respaldar.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+respaldarBD(){
+	# $1 : usuario de mysql
+	# $2 : path destino
+
+	local FECHA=$(date +"%d-%m-%y_%H-%M")
+	local USER=$1
+	local RUTA=$2
+
+	mysqldump --user="$USER" -p --all-databases > "$RUTA"/backupBD_"$FECHA".sql #Utilizo mysqldump para generar un backup de todas las bases de datos y lo envío a la ruta seleccionada
+	
+	if [ -s "$RUTA"/backupBD_"$FECHA".sql ] #Verifico que el backup creado no esté vacío
+	then
+		tput setaf 2
+		tput cup 7 0
+		sed -i 's/'$CANTB'/'$NUM'/' /etc/environment #Si se logra el resplado intercambio el valor anterior del contador
+		source /etc/environment
+        echo 0 
+	else
+		rm "$RUTA"/backupBD_"$FECHA".sql
+        echo 1
+	fi
+}
+respaldarDirectorios(){
+   	# $1 : ruta respaldo
+
+	local FECHA=$(date +"%d-%m-%y_%H-%M")
+	local RUTA
+
+    if [ $1 ]; then
+        RUTA=$1    
+    else
+        RUTA=$PATHD
+    fi
+
+  	if [ -d "$RUTA"/backupDIR_"$FECHA"/ ] #Verifico si existe el directorio del backup
+    then
+        echo 1 # la copia ya existe
+    else
+        mkdir "$RUTA"/backupDIR_"$FECHA"/
+		cp -r /home/USUARIOS "$RUTA"/backupDIR_"$FECHA"
+        echo 0 # se ha creado la copia
+    fi
+
+}
