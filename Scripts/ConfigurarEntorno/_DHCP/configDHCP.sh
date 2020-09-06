f17d50f926932be062b74d29a65eecdc1c46e660
AlexisUTU <alexis333utu@gmail.com>
2020-09-06 00:14:57 -0300

    Agrego ifcfg-enp0s3 por defecto

--- Scripts/ConfigurarEntorno/_DHCP/configDHCP.sh
+++ Scripts/ConfigurarEntorno/_DHCP/configDHCP.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/bash
-
-#targetaDeRed1="10.0.2.15/24"
-#targetaDeRed2="10.0.2.15/"
-#ip a flush dev eth0
-#ip a add 127.0.0.1/8 dev lo
-#ip a add 10.0.2.15/24 dev enp0s3
 #necesita root 
-
-
+# NO FUNCIONAL
+#nmtui
 echo "DHCPDARGS=enp0s3" >> /etc/sysconfig/dhcpd
-cp "/Scripts/ConfigurarEntorno/_DHCP/configuracionDHCPD.conf"  /etc/dhcp/dhcpd.conf 
+cp "/Scripts/ConfigurarEntorno/_DHCP/ifcfg-enp0s3" /etc/sysconfig/network-scripts/ifcfg-enp0s3 
+cp "/Scripts/ConfigurarEntorno/_DHCP/dhcpd.conf" /etc/dhcp/dhcpd.conf 
+
 firewall-cmd --add-service=dhcp --permanent 
-firewall-cmd --reload 
+firewall-cmd --reload
+
 systemctl restart network
 systemctl restart dhcpd
