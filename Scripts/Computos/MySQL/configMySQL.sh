4e127490fdfeca2ec6459bb845b7c6c6414f42a3
AlexisUTU <alexis333utu@gmail.com>
2020-10-04 15:05:35 -0300

    Agrego opcion de banner a SSH

--- Scripts/Computos/MySQL/configMySQL.sh
+++ Scripts/Computos/MySQL/configMySQL.sh
@@ -377,4 +377,3 @@ importarBD() {
 
 }
 #endregion
-importarBD
--- Scripts/ConfigurarEntorno/SSH/VConfigSSH.sh
+++ Scripts/ConfigurarEntorno/SSH/VConfigSSH.sh
@@ -14,7 +14,6 @@ pantallaSSH() {
     
     dibujarRectangulo 11 4 80 40 7 0
     dibujarTxt "CONFIGURANDO SSH" 44 7 0 7
-    dibujarTxt "Ejemplo: /Scripts/ConfigurarEntorno/SSH/bannerDefectoSSH.txt" 11 8 0
 
     dibujarTxt "USUARIOS PERMITIDOS (USER1,USER2...)" 36 11 0 7
     dibujarEntradaTxt 36 12 31 false
@@ -22,8 +21,9 @@ pantallaSSH() {
     dibujarTxt "PERMITIR USUARIOS ROOT" 40 16 0 7
     dibujarSwitch 36 17 30 3 $root
 
-    dibujarTxt "PATH DEL BANNER (none y no agrega el banner)" 11 11 0
-    dibujarEntradaTxt 11 12 80 false "$pathBanner"
+    dibujarTxt "PATH DEL BANNER (none y no agrega el banner)" 30 20 0 7
+    dibujarTxt "Ejemplo: /Scripts/ConfigurarEntorno/SSH/bannerDefectoSSH.txt" 20 21 0 7
+    dibujarEntradaTxt 11 22 80 false "$pathBanner"
 
     dibujarBoton "CONFIGURAR" 27 24 50 3
 
@@ -57,13 +57,17 @@ pantallaSSH() {
             "3") 
                 if $respuestaGestor;
                 then
-                    cp "$pathBanner" "/etc/bannerSSH.txt"
-                    pathBanner="/etc/bannerSSH.txt"
-                    chmod 755 "$pathBanner"
 
-                    configurarSSH $root $pathBanner
+                    if [ "$pathBanner" ];
+                    then 
+                        cp "$pathBanner" "/etc/bannerSSH.txt"
+                        pathBanner="/etc/bannerSSH.txt"
+                        chmod 755 "$pathBanner"
+                    fi
+                    configurarSSH "$root" "$pathBanner"
                     mensajeError "EL PUERTO POR DEFECTO SERÁ EL 2022" 2 34 33 5 5 2 2
                     continuar=false
+
                 fi   
                 ;;  
             *)
@@ -72,7 +76,6 @@ pantallaSSH() {
     done
     cerrarPantalla
 }
-
 configurarSSH () {
     # $1 : root
     # $2 : pathBanner
@@ -90,7 +93,10 @@ configurarSSH () {
         echo "AllowUsers ${usuarios/,/ }">>/etc/ssh/sshd_config
         sed -i "s/#PermitRootLogin yes/PermitRootLogin "$root"/" /etc/ssh/sshd_config
     fi
-    echo "Banner $pathBanner">>/etc/ssh/sshd_config
+    if [ "$pathBanner" ];
+    then
+        echo "Banner $pathBanner">>/etc/ssh/sshd_config
+    fi
     semanage port -a -t ssh_port_t -p tcp 2022 > /dev/null 2>&1
     systemctl restart sshd > /dev/null 2>&1
 }
